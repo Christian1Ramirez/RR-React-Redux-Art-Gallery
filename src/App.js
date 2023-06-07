5d288aae5fb8f9c988d7b7042c00557d4fac9b36
Chrisitian J Ramirez <slipkmetall@yahoo.com>
2023-06-06 19:32:13 -0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,44 +1,83 @@
-import './App.css';
-import { useSelector, useDispatch } from 'react-redux'
-import { useEffect } from 'react';
+import "./App.css";
+import { useSelector, useDispatch, connect } from "react-redux";
+import { useEffect } from "react";
+import {
+ fetchData,
+ nextImage,
+ prevImage,
+ setArtId,
+ reset,
+} from "./features/dataSlice";
 
-function App(props) {
-  const dispatch = useDispatch()
-  const data = useSelector((state) => state.data)
+const mapStateToProps = state => state.data;
 
-  const renderImg = () => {
-    if(data?.apiData) {
-      return <img style={{'width': '100vw'}} src={data.apiData.primaryImage} alt={data.apiData.title} />
-    } else {
-      return <p>image here</p>
-    }
+function App({ artId }) {
+ const dispatch = useDispatch();
+ const data = useSelector((state) => state.data);
+
+ const renderImg = () => {
+  if (data?.apiData) {
+   return (
+    <img
+     style={{ width: "100vw" }}
+     src={data.apiData.primaryImage}
+     alt={data.apiData.title}
+    />
+   );
+  } else {
+   return <p>image here</p>;
   }
+ };
 
-  return (
-    <div className="App">
-      <div>
-        <button onClick={() => {
-          // dispatch fetchData
-        }}>Thunk!</button>
-        <button onClick={() => {
-          // dispatch reset
-        }}>Clear</button>
-        <button onClick={() => {
-          // dispatch next
-        }}>Next</button>
-        <button onClick={() => {
-          // dispatch prev
-        }}>Back</button>
-      </div>
-      <input value={ data?.artId } onChange={(e) => {
-        // dispatch setArtId
-      }} />
-      <div>
-        {data?.artId}
-        {renderImg()}
-      </div>
-    </div>
-  );
+ useEffect(() => {
+  dispatch(fetchData())
+ }, [artId, dispatch]);
+
+ return (
+  <div className="App">
+   <div>
+    <button
+     onClick={() => {
+      dispatch(fetchData());
+     }}
+    >
+     Thunk!
+    </button>
+    <button
+     onClick={() => {
+      dispatch(reset());
+     }}
+    >
+     Clear
+    </button>
+    <button
+     onClick={() => {
+      dispatch(nextImage());
+     }}
+    >
+     Next
+    </button>
+    <button
+     onClick={() => {
+      dispatch(prevImage());
+     }}
+    >
+     Back
+    </button>
+   </div>
+   <input
+    value={data?.artId}
+    onChange={(e) => {
+     // dispatch setArtId
+     dispatch(setArtId(e.target.value));
+    }}
+   />
+   <div>
+    {data?.artId}
+    {renderImg()}
+   </div>
+  </div>
+ );
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
